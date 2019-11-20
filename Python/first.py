2d8797fef8f8b2dd1509af6f8b252564732c3dbc
niqibuqiwclgdj <2323302635qq.com>
2019-11-20 15:22:19 +0800

    www

--- Python/first.py
+++ Python/first.py
@@ -1,8 +1,7 @@
-name = "zhouan"
-age = 20
-hobbies = ["computerGames" , "playBalls" , "zky"]
-money = "millon"
-
-for hobb in hobbies:
-	print("Ny name is " + name + ",\n" + "I'm %d years old" %(age) + ",\n" + "and i love to play " + hobb)
-print("Ok , end it")
+school = {"zhouan":[100 , 90 , 111 , 120] , "wulaodi":[99 , 99 , 99 , 99] , "jidao":[11 , 112 , 123 , 134]}
+for name , grades in school.items():
+	print(name + "的语文为：" + str(grades[0]))
+	print(name + "的数学为：" + str(grades[1]))
+	print(name + "的英语为：" + str(grades[2]))
+	print(name + "的编程为：" + str(grades[3]))
+		
---
+++ focus/js/main.js
@@ -0,0 +1,64 @@
+/* var checklist = document.getElementById("checklist")
+
+var items = checklist.querySelectorAll("li");
+var inputs = checklist.querySelectorAll("input");
+
+for(var i = 0 ; i < items.length ; i++){
+	items[i].addEventListener("click" , editItem);
+	inputs[i].addEventListener("blur" , updateItem);
+	inputs[i].addEventListener("keypress" , itemKeypress);
+}
+
+function editItem(){
+	this.className = "edit";
+	var input = this.querySelector("input");
+	input.focus();
+	console.log(input.value.length)
+	input.setSelectionRange(0 , input.value.length)
+}
+
+function updateItem(){
+	this.previousElementSibling.innerHTML = this.value;
+	this.parentNode.className = "";
+}
+
+function itemKeypress(event){
+	if(event.keyCode == 13){
+		updateItem.call(this);
+	}
+}  */
+//文本全部加载完后获取checklist并为其赋值
+var checklist = document.getElementById("checklist")
+
+//取出所有在checklist里的li和input属性
+var items = checklist.querySelectorAll("li")
+console.log(items)
+var inputs = checklist.querySelectorAll("input")
+
+//循环，为每个都添加各自的函数操作
+for (var i = 0 ; i < items.length ; i ++){
+	items[i].addEventListener("click" , editItem)
+	inputs[i].addEventListener("blur" , updateItem)
+	inputs[i].addEventListener("keypress" , itemKeypress)
+}
+
+function editItem(){
+	this.className = "edit";
+	var input = this .querySelector("input");
+	//鼠标聚焦
+	input.focus();
+	input.setSelectionRange(0 , input.value.length)
+}
+
+//文本被修改时的函数
+function updateItem(){
+	this.previousElementSibling.innerHTML = this.value;
+	this.parentNode.className = "";
+}
+
+//鼠标按下时的函数
+function itemKeypress(){
+	if(event.keyCode == 13){
+		updateItem.call(this);
+	}
+}
